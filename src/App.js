56d10160fbe941e08053bd3e77db0087f1fe8151
Daniel.Nava <dnmboy@gmail.com>
2018-02-15 12:13:17 -0600

    Responsive design added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
 import LocationList from './components/LocationList';
 import './App.css';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const cities = [
   'Buenos Aires,ar',
@@ -20,10 +23,24 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <LocationList cities={cities} 
-            onSelectedLocation={this.handleSelectedLocation}></LocationList>
-        </div>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <AppBar title="Weatherapp" />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList cities={cities} 
+              onSelectedLocation={this.handleSelectedLocation}></LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper zDepth={4}>
+                <div className="detail"></div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
       </MuiThemeProvider>
     );
   }
