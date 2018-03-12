fffda19ee5ee538250cb92e4d7cb3ef0294a51a5
Daniel.Nava <dnmboy@gmail.com>
2018-03-12 17:45:03 -0600

    ForecastExtended and ForecastItem components added

---
+++ src/components/ForecastExtended.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
+import './styles.css';
+
+const days =[
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+];
+const data = {
+    temperature: 10,
+    humidity: 10,
+    weatherState: 'normal',
+    wind: 'normal',
+};
+
+class ForecastExtended extends Component{
+
+    renderForecastItemDays(){
+        return days.map( day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>));
+        
+    }
+
+    render() {
+        const { city } = this.props;
+
+        return(
+            <div>
+                <h2 className="forecast-title">Pronóstico Extendido para {city}</h2>
+                {this.renderForecastItemDays()}
+            </div>
+        );
+    }
+}
+
+ForecastExtended.propTypes = {
+    city: PropTypes.string.isRequired,
+}
+export default ForecastExtended;
